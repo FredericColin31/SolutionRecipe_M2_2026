2a6303e5b6d6db56fc314578308882317e5a56f0
Frédéric COLIN <frederic.colin@equadex.net>
2026-09-08 16:05:18 +0200

    Step 5

---
+++ Services/Xml2RecipesServices.cs
@@ -0,0 +1,22 @@
+﻿using DataContracts;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Services
+{
+    public class Xml2RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            var xdoc = new XDocument();
+
+            xdoc = XDocument.Load("recipes.xml");
+
+            return xdoc.Descendants("recipe").Select(node => new Recipe() { Id = Guid.Parse(node.Attribute("id")?.Value ?? Guid.Empty.ToString()), Title = node.Attribute("title")?.Value ?? string.Empty }).ToList();
+        }
+    }
+}
