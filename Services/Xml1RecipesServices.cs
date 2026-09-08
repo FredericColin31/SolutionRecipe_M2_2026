4b7d201f4ac588973c0201ee03fd2d44b68e3e36
Frédéric COLIN <frederic.colin@equadex.net>
2026-09-08 14:57:04 +0200

    Step 04 b

---
+++ Services/Xml1RecipesServices.cs
@@ -0,0 +1,16 @@
+﻿using DataContracts;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class Xml1RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            // System.Xml
+        }
+    }
+}
