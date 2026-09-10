aa1fcda6f7d60f63b8549fb25c94cb8f2a9d8ad6
Frédéric COLIN <frederic.colin@equadex.net>
2026-09-10 09:01:45 +0200

    Step 08

---
+++ Services/Db04RecipesServices.cs
@@ -0,0 +1,19 @@
+﻿using DataContracts;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class Db04RecipesServices : AbstractRecipesServices
+    {
+        public override List<Recipe> GetAll()
+        {
+            using (var context = new DataAccessLayer.BRecipesContext())
+            {
+                return context.Recipes.Select(@recipe => new DataContracts.Recipe() { Id = @recipe.Id, Title = @recipe.Title }).ToList();
+            }
+        }
+    }
+}
