4ff95846ebbcfb83506a50ecc77f023ac285029d
Frédéric COLIN <frederic.colin@equadex.net>
2026-09-10 09:16:07 +0200

    Step 09

--- Services/DataAccessLayer/BRecipesContext.cs
+++ Services/DataAccessLayer/BRecipesContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Services.Core;
 
 namespace Services.DataAccessLayer;
 
@@ -21,7 +22,7 @@ public partial class BRecipesContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=bRecipes;User id=sa;Password=Formation@31;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("RecipesConnectionString".GetConnectionStringFor());
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
